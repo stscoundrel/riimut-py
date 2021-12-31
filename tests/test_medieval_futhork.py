a19e2c767114f85bfc9696b62486063405e21489
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-12-31 11:14:13 +0200

    Fix minor lint warns

--- tests/test_medieval_futhork.py
+++ tests/test_medieval_futhork.py
@@ -18,7 +18,7 @@ def test_transforms_runes_to_letters():
 
 
 def test_transforms_later_old_norse_to_runes():
-     # From Lord's Prayer, in Old Norse.
+    # From Lord's Prayer, in Old Norse.
     content = "Faðer uor som ast i himlüm, halgað warðe þit nama"
     expected = "ᚠᛆᚦᚽᚱ:ᚢᚮᚱ:ᛋᚮᛘ:ᛆᛋᛏ:ᛁ:ᚼᛁᛘᛚᚢᛘ,:ᚼᛆᛚᚵᛆᚦ:ᚠᛆᚱᚦᚽ:ᚦᛁᛏ:ᚿᛆᛘᛆ"
     result = medieval_futhork.letters_to_runes(content)
@@ -29,7 +29,7 @@ def test_transforms_later_old_norse_to_runes():
 def test_transforms_runes_to_later_old_norse():
     # From Lord's Prayer, in Old Norse.
     content = "ᚠᛆᚦᚽᚱ:ᚢᚮᚱ:ᛋᚮᛘ:ᛆᛋᛏ:ᛁ:ᚼᛁᛘᛚᚢᛘ:ᚼᛆᛚᚵᛆᚦ:ᚠᛆᚱᚦᚽ:ᚦᛁᛏ:ᚿᛆᛘᛆ"
-    expected = "faþer uor som ast i himlum halgaþ farþe þit nama" # Wont tell apart eth & thorn in mid sentence.
+    expected = "faþer uor som ast i himlum halgaþ farþe þit nama"  # Wont tell apart eth & thorn in mid sentence.
     result = medieval_futhork.runes_to_letters(content)
 
     assert result == expected
