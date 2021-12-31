a069dc37f2eaf70220f4047354ee73defc6f8e12
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-12-31 09:27:08 +0200

    Populate readme & setup.py details

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="projectname",
+    name="riimut",
     version="1.0.0",
     author="stscoundrel",
-    description="Project desc",
+    description="Transform latin letters to runes and vice versa",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/stscoundrel/python-template",
+    url="https://github.com/stscoundrel/riimut-py",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.9",
