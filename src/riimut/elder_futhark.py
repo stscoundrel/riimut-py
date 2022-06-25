316140d43541bac39f8fb726b8d07f97a512b199
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-25 14:33:58 +0300

    Make mappings final constants, make transform private

--- src/riimut/elder_futhark.py
+++ src/riimut/elder_futhark.py
@@ -1,13 +1,13 @@
-from .transform import transform
+from .transform import _transform
 from .dialects.elder_futhark.letters import get_letter_mapping
 from .dialects.elder_futhark.runes import get_rune_mapping
 
 
 def letters_to_runes(content: str) -> str:
     mapping = get_letter_mapping()
-    return transform(content, mapping)
+    return _transform(content, mapping)
 
 
 def runes_to_letters(content: str) -> str:
     mapping = get_rune_mapping()
-    return transform(content, mapping)
+    return _transform(content, mapping)
