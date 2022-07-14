24db9dbc8563a05ca132e2d4b8e9d6ec922df8b4
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2022-07-14 16:08:24 +0300

    v1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="riimut",
-    version="1.1.1",
+    version="1.2.0",
     author="stscoundrel",
     description="Transform latin letters to runes and vice versa",
     long_description=long_description,
